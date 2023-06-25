7ba9f85b774acfabc88b62c2fb29bc3f46919fc9
puff <>
2023-06-25 21:15:17 +0200

    ajout saveAS + choix path + file name

--- ytdl_mp3.py
+++ ytdl_mp3.py
@@ -2,26 +2,41 @@ from pytube import YouTube
 import os
 from tkinter import *
 from tkinter import ttk
-
+from tkinter.filedialog import asksaveasfilename
 
 def convert():
     try:
         youtube = YouTube(url.get())
         errorMSG.grid_forget()
-        # 140 correspond au .mp4 audio suelement en 128kb/s
-        video = youtube.streams.get_by_itag(140)
-        outfile = video.download('/home/puff/Musique')
-
+        
+        video = youtube.streams.get_by_itag(140)# 140 correspond au .mp4 audio suelement en 128kb/s
+        #outfile = video.download(output_path='/home/puff/Musique/', filename=file_name)
+        global file_name
+        global file_path
+        if not file_name:
+            file_name = youtube.title + '.mp3'
+        if not file_path:
+            file_path = '/home/puff/Musique'
+        video.download(output_path=file_path, filename=file_name)
         # rename fichier de sortie
-        base, ext = os.path.splitext(outfile)
-        newfile = base + '.mp3'
-        os.rename(outfile, newfile)
+        #base, ext = os.path.splitext(outfile)
+        #newfile = base + '.mp3'
+        #print(newfile)
+        #os.rename(outfile, newfile)
     except:
-        errorMSG.grid(column=0, row=3, columnspan=2)
+        errorMSG.grid(column=0, row=4, columnspan=2)
+
+def savePath():
+    path = asksaveasfilename(defaultextension='.mp3')
+    if not path:
+        return
+    global file_name
+    global file_path
+    file_path, file_name = os.path.split(path)
 
 
 window = Tk()
-window.geometry("500x150")
+window.geometry("500x300")
 window.resizable(False,False)
 window.title("Youtube Downloader")
 frm = ttk.Frame(window)
@@ -29,12 +44,15 @@ frm.grid()
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
 
+url = StringVar()
+file_path = str()
+file_name = str()
+
 text = ttk.Label(frm, text="Lien de la vidéo:")
 text.grid(column=1)
 text.grid_rowconfigure(1, weight=1)
 text.grid_columnconfigure(1, weight=1)
 
-url = StringVar()
 
 url_textbox = ttk.Entry(frm, width=50, textvariable=url)
 url_textbox.focus()
@@ -42,8 +60,13 @@ url_textbox.grid(column=1, row=1, pady=20)
 url_textbox.grid_rowconfigure(1, weight=1)
 url_textbox.grid_columnconfigure(1, weight=1)
 
+browse = ttk.Button(frm, text='Parcourir', command=savePath)
+browse.grid(column=1, row=2)
+browse.grid_rowconfigure(1, weight=1)
+browse.grid_columnconfigure(1, weight=1)
+
 convertir = ttk.Button(frm, text="Convertir", command=convert)
-convertir.grid(column=1,row=2)
+convertir.grid(column=1,row=3)
 convertir.grid_rowconfigure(1, weight=1)
 convertir.grid_columnconfigure(1, weight=1)
 
